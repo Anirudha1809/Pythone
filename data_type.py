619fc18a20e303e8b600414b1a8d3fc69210c01c
Anirudha1809 <anirudha1809@gmail.com>
2024-02-10 01:35:03 +0530

    data types

---
+++ data_type.py
@@ -0,0 +1,20 @@
+"""a=10
+b=20
+c=6+2j
+s='hi i am ani and doing python'
+print(a,(type(a)))
+print (type(a))
+print(b)
+print(type(b))
+print(c)
+print(c.real)
+print(c.imag)
+print(type(c))
+print(s)
+print(type(s))"""
+
+b=True
+print(type(b))
+print(b)
+f=5.5
+print(f)
