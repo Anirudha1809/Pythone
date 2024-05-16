34b12b24d1600387242925454a7e57eda82e3448
Anirudha1809 <anirudha1809@gmail.com>
2024-05-17 00:50:01 +0530

    commit all

--- .ipynb_checkpoints/rev_str-checkpoint.py
+++
@@ -1,8 +0,0 @@
-'''str=input("enter a string:")
-print("before reverse the string",str)
-rev_str=str[-1::-1]
-print("after reverse the string",rev_str)'''
-
-celcius=float(input("celcius value:"))
-ferhenite=float(9/5*celcius+32)
-print("ferhenite value is %f",ferhenite)
